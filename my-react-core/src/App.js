ddd47059dacafc2e8e9a702a329e6a237d3c5c71
wujiabao <wujiabao@wujiabaodeMacBook-Pro.local>
2025-05-09 18:05:50 +0800

    1

---
+++ my-react-core/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [a, setA] = useState("b");
+
+  return (
+    <div className="App">
+      谢谢1
+    </div>
+  );
+}
+
+export default App;
